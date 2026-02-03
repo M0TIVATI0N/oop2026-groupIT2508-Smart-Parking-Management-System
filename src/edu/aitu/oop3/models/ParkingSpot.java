18cd7e0b6fd909839dfa86601985aa5e034e04d6
M0TIVATI0N <khasenovdt@gmail.com>
2026-02-03 18:24:02 +0500

    Dauit Khassenov - refined the base model to support inheritance for specialized spot types (Standard, Disabled, Electric).

--- src/edu/aitu/oop3/models/ParkingSpot.java
+++ src/edu/aitu/oop3/models/ParkingSpot.java
@@ -4,11 +4,13 @@ public class ParkingSpot {
     private final int id;
     private final Tariff tariff;
     private final boolean isReserved;
+    private final String type;
 
-    public ParkingSpot(int id, Tariff tariff, boolean isReserved) {
+    public ParkingSpot(int id, Tariff tariff, boolean isReserved, String type) {
         this.id = id;
         this.tariff = tariff;
        this.isReserved = isReserved;
+        this.type = type;
     }
 
     public int getId() {
@@ -20,4 +22,5 @@ public class ParkingSpot {
     public boolean isReserved() {
         return isReserved;
     }
+    public String getType() { return type; }
 }
